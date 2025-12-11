ee5e777af0d70e4cf3572f87929608a99ce95e47
mohammad <mmdbazooka@gmail.com>
2025-12-12 00:11:51 +0330

    replace download app => Login/Signup

--- src/components/shared/Header/DesktopNavbar.tsx
+++ src/components/shared/Header/DesktopNavbar.tsx
@@ -1,23 +1,50 @@
+import { LogIn } from "lucide-react";
 import { MotionSpan } from "@/utils/MotionWrapper";
-import { Download } from "lucide-react";
+// import dynamic from "next/dynamic";
+import { DesktopNavbarProps } from "@/types/main";
+import { useState } from "react";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 
-const DesktopNavbar: React.FC<any> = ({
+// const CartSidebar = dynamic(
+//   () => import("@/components/main/CartSidebar").then((mod) => mod.default),
+//   { ssr: false }
+// );
+
+const DesktopNavbar: React.FC<DesktopNavbarProps> = ({
   isAuthenticated,
   user,
   pathname,
   openLoginDialog,
   setOpenLoginDialog,
-  links,
-  handleScroll,
-  activeId
 }) => {
+  const [activeId, setActiveId] = useState<string>("home");
   const t = useTranslations("navbar");
+  const a = useTranslations("adminLogin");
+  const router = useRouter();
+  const links = [
+    { href: "home", label: t("desktopNavbar.home") },
+    { href: "about-us", label: t("desktopNavbar.aboutUs") },
+    { href: "HowItWorks", label: t("desktopNavbar.howItWorks") },
+    { href: "contact-us", label: t("desktopNavbar.features") },
+    { href: "faq", label: t("desktopNavbar.frequentlyAskedQuestions") },
+  ];
+
+  const handleScroll = (href: string) => {
+    const el = document.getElementById(href);
+    if (!el) return;
+
+    const offset = href === "home" ? 0 : -100;
+    const top = el.getBoundingClientRect().top + window.scrollY + offset;
+
+    window.scrollTo({ top, behavior: "smooth" });
+    setActiveId(href);
+  };
 
   return (
     <>
       <div data-testid="main-nav" className="hidden lg:flex items-center gap-4">
-        {links?.map(({ href, label }: any) => {
+        {links?.map(({ href, label }) => {
           const isActive = activeId === href;
 
           return (
@@ -43,9 +70,26 @@ const DesktopNavbar: React.FC<any> = ({
       </div>
 
       <div className="flex items-center gap-3 h-full">
-        <div className="bg-white text-bodyDark h-full px-4 rounded-full font-medium flex items-center gap-2 text-[15px] cursor-pointer max-lg:border max-lg:border-action">
-          <Download width={18} className="max-lg:stroke-action-hover" />{" "}
-          {t("desktopNavbar.downloadApp")}
+        {/* <div className="hidden md:block">
+          <ThemeSwitcher />
+        </div> */}
+        {/* <CartSidebar />
+        <UserDropdown
+          user={user}
+          isAuthenticated={isAuthenticated}
+          onLoginClick={() => setOpenLoginDialog(true)}
+        />
+        <LoginForm
+          open={openLoginDialog}
+          onOpenChange={setOpenLoginDialog}
+          onSuccess={() => setOpenLoginDialog(false)}
+        /> */}
+        <div
+          className="bg-white text-bodyDark h-full px-4 rounded-full font-medium flex items-center gap-2 text-[15px] cursor-pointer max-lg:border max-lg:border-action"
+          onClick={() => router.push("/admin-login")}
+        >
+          <LogIn width={18} className="max-lg:stroke-action-hover" />{" "}
+          {a("loginRegister")}
         </div>
       </div>
     </>
